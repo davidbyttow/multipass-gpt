fe55b27da9f2e0d4cc0e15d5bd646921bdc3a99e
David Byttow <david@sorare.com>
2023-04-14 13:58:45 -0500

    text drawings

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from typing import Dict, List
 from quart import Quart, request, jsonify, send_file
 
-import replicate
 import quart_cors
+import replicate
 
 app = Quart(__name__)
 quart_cors.cors(app, allow_origin="https://chat.openai.com")
 
 # requires REPLICATE_API_TOKEN env var to be set
 
-SD_CHECKPOINT = "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf"
-DAMO_TEXT_TO_VIDEO_CHECKPOINT = "cjwbw/damo-text-to-video:1e205ea73084bd17a0a3b43396e49ba0d6bc2e754e9283b2df49fad2dcf95755"
+STABLE_DIFFUSION_MODEL = "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf"
+DAMO_TEXT_TO_VIDEO_MODEL = "cjwbw/damo-text-to-video:1e205ea73084bd17a0a3b43396e49ba0d6bc2e754e9283b2df49fad2dcf95755"
+CONTROL_NET_SCRIBBLE_MODEL = "jagilley/controlnet-scribble:435061a1b5a4c1e26740464bf786efdfa9cb3a3ac488595a2de23e143fdb0117"
 
 
 @app.route("/create_image", methods=["POST"])
@@ -28,14 +29,49 @@ async def create_image():
     # seed=None
 
     output = replicate.run(
-        SD_CHECKPOINT,
+        STABLE_DIFFUSION_MODEL,
         input={"prompt": prompt},
     )
 
     if len(output) == 0:
-        return jsonify({"status": "Failed to create image"})
+        return jsonify({"status": "failed to create image"})
 
     image = output[0]
+    return jsonify(
+        {
+            "status": "image created",
+            "result": image,
+        }
+    )
+
+
+@app.route("/create_image_from_drawing", methods=["POST"])
+async def create_image_from_drawing():
+    data = await request.json
+    prompt = data["prompt"]
+    image_url = data["image_url"]
+
+    # other options:
+    # num_samples=1
+    # ddim_steps=20
+    # scale=9
+    # eta=None
+    # a_prompt="best quality, extremely detailed"
+    # n_prompt="longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality"
+    # seed=None
+
+    output = replicate.run(
+        CONTROL_NET_SCRIBBLE_MODEL,
+        input={
+            "image": image_url,
+            "prompt": prompt,
+        },
+    )
+
+    if len(output) != 2:
+        return jsonify({"status": "failed to create image"})
+
+    image = output[1]
     return jsonify(
         {
             "status": "Image created",
@@ -56,12 +92,12 @@ async def create_video():
     # seed=None
 
     output = replicate.run(
-        DAMO_TEXT_TO_VIDEO_CHECKPOINT,
+        DAMO_TEXT_TO_VIDEO_MODEL,
         input={"prompt": prompt},
     )
 
     if not output:
-        return jsonify({"status": "Failed to create video"})
+        return jsonify({"status": "failed to create video"})
 
     return jsonify(
         {
